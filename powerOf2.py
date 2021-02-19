4601b53544728cfd397756a14afa165195495049
AdityaJsr <adityajsr1996@gmail.com>
2021-02-19 15:25:25 +0530

    powerOf2.py: The code is complete with all edge cases considered

--- powerOf2.py
+++ powerOf2.py
@@ -1,9 +1,15 @@
 def exponent():
-    user_input = input("Enter a exponent factor: ")
-    expo = int(user_input)
-    if(expo>=0)and (expo<= 31):
-        output = pow(2, expo)
-        print("The output for the above expo facor is:"+str(output))
-
+    user_input = input("Enter a exponent value: ")
+    if(user_input.isdigit()):
+        expo = int(user_input)
+        if(expo>=0) and (expo<= 31):
+            output = pow(2, expo)
+            print("The output for the above expo facor is:"+str(output))
+        else:
+            print("The exponent value cannot be less than 0 or more than 31")
+            exponent()
+    else:
+        print("The input has to be an integer")
+        exponent()
 if __name__ == "__main__":
     exponent()
