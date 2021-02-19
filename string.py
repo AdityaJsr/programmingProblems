0202459fb7ff2b253cd19b146a67669152ebd72b
AdityaJsr <adityajsr1996@gmail.com>
2021-02-19 15:15:43 +0530

    string.py: Complete code with edge cases considered

--- string.py
+++ string.py
@@ -1,8 +1,14 @@
-# First Project 
+"""
+ First Project 
+This is a basic program for string replacement
+"""
 def replace():
     user_input = input("Enter the User name :")
     if(len(user_input) >= 3):
-        print("Hello "+user_input+" , How are you?")
+        if (user_input[0][0].isdigit()):
+            print("invalid Username: usernames cannot start with a number")
+        else:
+            print("Hello "+user_input+" , How are you?")
     else:
         print("Invalid user name\nminimum 3 characters")
         replace()
