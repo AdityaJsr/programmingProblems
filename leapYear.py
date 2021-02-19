93e1210dc1e74c9345957c4433a8712559ce1d52
AdityaJsr <adityajsr1996@gmail.com>
2021-02-19 14:55:21 +0530

    leapYear.py: the code is completely functional with all edge cases considered

--- leapYear.py
+++ leapYear.py
@@ -1,26 +1,25 @@
-# def leapYear():
-    
-user_input = input("Enter the Year : ")
-input_lenghth = len(user_input)
-# print(len)
-year = int(user_input)
-print(year)
-if (input_lenghth == 4):
-    print("the length statement works ")
-#         if(year%4 == 0):
-#             if(year%100 == 0):
-#                 if(year%400 == 0 ):
-#                     print("-----"+year+" is a Leap year-----")
-#                 else:
-#                     print("-----"+year+" is not a Leap year-----")
-#             else:
-#                 print("-----"+year+" is not a Leap year-----")
-#         else:
-#             print("-----"+year+" is not a Leap year-----")
-#     else:
-#         print("please enter a valid year :")
-#         leapYear()
+def leapYear():
+    user_input = input("Enter the Year : ")
+    input_lenghth = len(user_input)
+    if user_input.isdigit():
+        year = int(user_input)
+        if (input_lenghth == 4):
+            if(year%4 == 0):
+                if(year%100 == 0):
+                    if(year%400 == 0 ):
+                        print("-----"+user_input+" is a Leap year-----")
+                    else:
+                        print("-----"+user_input+" is not a Leap year-----")
+                else:
+                    print("-----"+user_input+" is not a Leap year-----")
+            else:
+                print("-----"+user_input+" is not a Leap year-----")
+        else:
+            print("please enter a valid year :")
+            leapYear()
+    else:
+        print("years can be entered only as a numeric value :")
+        leapYear()
 
-# if __name__ == "__main__":
-#     leapYear()
-
+if __name__ == "__main__":
+    leapYear()
