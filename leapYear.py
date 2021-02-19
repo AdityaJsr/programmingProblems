95aa4aede437f48b2f21c625221e3b84aeba96a9
AdityaJsr <adityajsr1996@gmail.com>
2021-02-19 14:50:38 +0530

    leapYear.py: First commit

--- leapYear.py
+++ leapYear.py
@@ -1,7 +1,12 @@
 def leapYear():
-    year = input("Enter the Year : ")
-    if (len(year) == 4):
-        
-        
-        
+    user_input = input("Enter the Year : ")
+    input_lenghth = len(user_input)
+    if user_input.isdigit():
+ 
+    else:
+        print("years can be entered only as a numeric value :")
+        leapYear()
+
+if __name__ == "__main__":
+    leapYear()
 
