604155980bdb189fc5a616650c2ab5b96a3bd267
AdityaJsr <adityajsr1996@gmail.com>
2021-02-19 14:53:23 +0530

    leapYear.py: Length detection works

--- leapYear.py
+++ leapYear.py
@@ -1,12 +1,26 @@
-def leapYear():
-    user_input = input("Enter the Year : ")
-    input_lenghth = len(user_input)
-    if user_input.isdigit():
- 
-    else:
-        print("years can be entered only as a numeric value :")
-        leapYear()
+# def leapYear():
+    
+user_input = input("Enter the Year : ")
+input_lenghth = len(user_input)
+# print(len)
+year = int(user_input)
+print(year)
+if (input_lenghth == 4):
+    print("the length statement works ")
+#         if(year%4 == 0):
+#             if(year%100 == 0):
+#                 if(year%400 == 0 ):
+#                     print("-----"+year+" is a Leap year-----")
+#                 else:
+#                     print("-----"+year+" is not a Leap year-----")
+#             else:
+#                 print("-----"+year+" is not a Leap year-----")
+#         else:
+#             print("-----"+year+" is not a Leap year-----")
+#     else:
+#         print("please enter a valid year :")
+#         leapYear()
 
-if __name__ == "__main__":
-    leapYear()
+# if __name__ == "__main__":
+#     leapYear()
 
