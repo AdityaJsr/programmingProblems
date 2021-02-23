73bb4f8a1d95134462cfdc6dd52246130dce0021
AdityaJsr <adityajsr1996@gmail.com>
2021-02-23 15:21:22 +0530

    tempConversion.py: added all the functions

--- piUnit/tempConversion.py
+++ piUnit/tempConversion.py
@@ -7,28 +7,28 @@ modified time - 23 ‎February ‎2021, ‏‎12:29:21
 """
 # The function for user_input() for choice.
 def user_input():
-    choice = int(input("Enter 1 if you want to convert Celsius to Fahrenheit : \n or 2 to convert Fahrenheit to Celsius :"))
-    # Returning the choice to convertor function().
-    return (choice)
+    choice = int(input("Enter 1 to convert Celsius to Fahrenheit : \nEnter 2 to convert Fahrenheit to Celsius :\n ch = : "))
+    if choice == 1:
+        celsiusToFahrenheit()
+    elif choice ==2:
+        fahrenhietToCelsius()
+    else:
+        print("Please enter a valid choice between 1 and 2 : ")
+        user_input()
+    
 
-# The function for temperature conversion.
-def convertor():
-    ch = int(user_input())
-    # Choice 1 for converting Celsius to Fahrenheit.
-    if (ch == 1):
-        temp = float(input("Enter the temperature in Celsius : "))
-        output = (temp*1.8)+32
-        print("The Fahrenheit equivalent of "+str(temp)+"°C = "+str(output)+"°F")
-    # Choice 2 for converting Fahrenheit to Celsius.
-    elif(ch == 2):
-        temp = float(input("Enter the temperature in Fahrenheit : "))
-        output = (temp - 32)*(5/9)
-        print("The Celsius equivalent of "+str(temp)+"°F = "+str(output)+"°C")
-    # # Error message for wrong choice.
-    # else:
-    #     print("select a proper value for conversion : ")
-    #     user_input()
+# The function for temperature conversion from Celsius to Fahrenheit.
+def celsiusToFahrenheit():
+    temp = float(input("Enter the temperature in Celsius : "))
+    output = int((temp*1.8)+32)
+    print("The Fahrenheit equivalent of "+str(temp)+"°C = "+str(output)+"°F")
+
+# The function for temperature conversion from Fahrenheit to Celsius.
+def fahrenhietToCelsius():
+    temp = float(input("Enter the temperature in Fahrenheit : "))
+    output = (temp - 32)*(5/9)
+    print("The Celsius equivalent of "+str(temp)+"°F = "+str(output)+"°C")
 
 # The main function.
 if __name__ == "__main__":
-    convertor()
+    user_input()
